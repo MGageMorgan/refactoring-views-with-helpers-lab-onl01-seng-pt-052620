1671ce150ee46a532b2a2241e4be62a21e0d9a55
M. Gage Morgan <gage@gages.blog>
2020-10-26 18:55:05 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist
+      link_to song.artist.name, "/artists/#{song.artist.id}"
+    else
+      link_to "Add Artist", "/songs/#{song.id}/edit"
+    end
+  end
 end
